6ee490eb59484ea55af8245cc1706cd3698f7f46
Vitor Rafael Leite <vitor.leite@totvs.com.br>
2023-05-22 15:11:45 -0300

    Renomeado os nomes e feitos os redirects.

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const open = (url) => {
+  window.open(url);
+};
+
+const openTwitter = () => open("https://twitter.com");
+const openInstagram = () => open("https://instagram.com");
+const openFacebook = () => open("https://facebook.com");
+const openMaps = () => open("https://www.google.com.br/maps");
